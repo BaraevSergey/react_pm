4c88b21b59dcf94ed4ff09a3781986b838ca0d71
Baraev Sergey <Baraev.sergey@mail.ru>
2021-02-04 20:50:22 +0500

    DeleteMyDevelopes NewStartPoint

--- src/App.js
+++ src/App.js
@@ -5,27 +5,9 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
-        <div>
-          <div class="header-page">
-            
-          </div>
-          
-          <div class='center-block'>
-            <div class="center top-frame-login">
-              <div class="row">
-                <div class="col">
-                  <LoginText/>
-                </div>
-              </div> 
-              <div class="row indent">
-                <div class="col">
-                  <InputPassword/>
-                </div>
-              </div>     
-            </div>
-
-          </div>
-        </div>
+      <div>
+        
+      </div>
   );
   
 }
