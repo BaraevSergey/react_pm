47813126320c7f37d9c3de2f6efe91dca5afe1f6
Baraev Sergey <Baraev.sergey@mail.ru>
2021-02-04 21:02:04 +0500

    comm

--- src/App.js
+++ src/App.js
@@ -1,26 +1,10 @@
 import React from 'react'
-import LoginText from './MyComponents/Login'
-import InputPassword from './MyComponents/Password'
 import 'bootstrap/dist/css/bootstrap.min.css';
 function App() {
-  const imgStyle = {
-    margin:'30%',
-  };
 
   return (
-        <div class='container-fluid' style={imgStyle}>
+        <div>
 
-          <div class="row" >
-            <div class="col-md-6 offset-md-3">
-              <LoginText/>
-            </div>
-          </div>
-
-          <div class="row">
-            <div class="col-md-6 offset-md-3">
-              <InputPassword/>
-            </div>
-          </div>
         </div>
   );
   
